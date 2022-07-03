2f23bb454b2109ae58c9141bf3134ad989ad6559
anquocviet <an.q.viet203@gmail.com>
2022-07-03 21:57:51 +0700

    Setup my lualine

--- lua/plugins-config/nvim-lualine/init.lua
+++ lua/plugins-config/nvim-lualine/init.lua
@@ -1,21 +1,29 @@
+local function logo()
+  return ' '
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
     component_separators = { left = '', right = ''},
-    section_separators = { left = ' ', right = ''},
+    section_separators = { left = ' ', right = ''},
     disabled_filetypes = {},
     always_divide_middle = true,
     globalstatus = false,
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
+    lualine_a = {
+        { logo , separator = { left = ''}, right_padding = 2 },
+        'mode'
+    },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = {
+        {'location', separator = { right = '' }}
+    },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
@@ -26,4 +34,5 @@ require('lualine').setup {
   },
   tabline = {},
   extensions = {}
+}
 }
