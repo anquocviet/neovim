5ca3b35da8b54231557bacba7e7ba8e1e7b44548
anquocviet <an.q.viet203@gmail.com>
2022-07-05 09:21:23 +0700

    Include file nvim-bufferline

--- init.lua
+++ init.lua
@@ -1,10 +1,13 @@
 require("colors")
 require("settings")
 require("plugins")
+
 require("plugins-config.nvim-tree")
 require("plugins-config.nvim-treesitter")
 require("plugins-config.nvim-autopairs")
 require("plugins-config.nvim-lualine")
+require("plugins-config.nvim-bufferline")
+
 require("keybinds")
 require("lsp-config.language-server")
 require("lsp-config.auto-completion")
