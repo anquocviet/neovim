1d5e401e81d31108e1d7744aca544cbd3d94d6ee
anquocviet <an.q.viet203@gmail.com>
2022-07-05 09:12:06 +0700

    Append keybinds for buffers

--- lua/keybinds/init.lua
+++ lua/keybinds/init.lua
@@ -7,5 +7,14 @@ vim.g.mapleader = ' '
 -- Disable 'Space' key
 map('n', '<Space>', '', opts)
 
+-- Toggle NvimTree
 map('n', '<leader>n', ':NvimTreeToggle<CR>', opts)
+
+-- Add new buffer and move to it
+map('n', '<Leader>t', ':tabnew ', { silent = false })
+-- Buffer pick functionality
+map('n', '<Leader>b', ':BufferLinePick<CR>', opts)
+-- Switch among buffers
+map('n', '<A-TAB>', ':bnext<CR>', {silent = false})
+map('n', '<S-TAB>', ':bprevious<CR>', opts)
 
