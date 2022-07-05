bd4316e6b31c392ca359191e5bb133566c7bdb57
anquocviet <an.q.viet203@gmail.com>
2022-07-05 09:37:47 +0700

    Add description

--- init.lua
+++ init.lua
@@ -1,13 +1,17 @@
-require("colors")
+-- Basic config
 require("settings")
+require("keybinds")
 require("plugins")
 
-require("plugins-config.nvim-tree")
-require("plugins-config.nvim-treesitter")
-require("plugins-config.nvim-autopairs")
-require("plugins-config.nvim-lualine")
-require("plugins-config.nvim-bufferline")
+-- Themes config
+require("colors")
 
-require("keybinds")
-require("lsp-config.language-server")
-require("lsp-config.auto-completion")
+-- Config plugins
+require("plugins-config.nvim-tree")         -- For File expoler
+require("plugins-config.nvim-treesitter")   -- For Highlight code
+require("plugins-config.nvim-autopairs")    -- For Auto pairs
+require("plugins-config.nvim-lualine")      -- For Status line
+require("plugins-config.nvim-bufferline")   -- For Buffer line
+
+require("lsp-config.language-server")       -- Language server config
+require("lsp-config.auto-completion")       -- Auto complete code
