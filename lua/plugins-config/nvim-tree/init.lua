65b36e25dfd95759c1b06d2abf1eb6182beb909e
anquocviet <an.q.viet203@gmail.com>
2022-07-06 20:24:19 +0700

    Append keybind toggle nvim-tree

--- lua/plugins-config/nvim-tree/init.lua
+++ lua/plugins-config/nvim-tree/init.lua
@@ -1,3 +1,10 @@
 require("nvim-tree").setup({
 })
 
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Toggle NvimTree
+map('n', '<leader>n', ':NvimTreeToggle<CR>', opts)
+
