8c6d9b8b8bd6af543800074abbb55f4e512c8cb9
anquocviet <an.q.viet203@gmail.com>
2022-07-06 20:23:15 +0700

    Append keybinds for buffers

--- lua/plugins-config/nvim-bufferline/init.lua
+++ lua/plugins-config/nvim-bufferline/init.lua
@@ -28,3 +28,16 @@ require('bufferline').setup {
         }
     }
 }
+
+
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Add new buffer and move to it
+map('n', '<Leader>t', ':tabnew ', { silent = false })
+-- Buffer pick functionality
+map('n', '<Leader>b', ':BufferLinePick<CR>', opts)
+-- Switch among buffers
+map('n', '<A-TAB>', ':bnext<CR>', {silent = false})
+map('n', '<S-TAB>', ':bprevious<CR>', opts)
