bca18800e8282288ca1229945f918ccfd8f12784
anquocviet <an.q.viet203@gmail.com>
2022-07-06 20:20:44 +0700

    Move keybinds of plugins to file config plugin

--- lua/keybinds/init.lua
+++ lua/keybinds/init.lua
@@ -1,4 +1,6 @@
 local map = vim.api.nvim_set_keymap
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
 local opts = { noremap = true, silent = true }
 
 -- Set 'Space' key as leader key
@@ -7,14 +9,8 @@ vim.g.mapleader = ' '
 -- Disable 'Space' key
 map('n', '<Space>', '', opts)
 
--- Toggle NvimTree
-map('n', '<leader>n', ':NvimTreeToggle<CR>', opts)
+-- Sync Plugin: `PackerUpdate` and then `PackerCompile`
+map('n', '<leader>ps', ':PackerSync<CR>', { silent = false})
 
--- Add new buffer and move to it
-map('n', '<Leader>t', ':tabnew ', { silent = false })
--- Buffer pick functionality
-map('n', '<Leader>b', ':BufferLinePick<CR>', opts)
--- Switch among buffers
-map('n', '<A-TAB>', ':bnext<CR>', {silent = false})
-map('n', '<S-TAB>', ':bprevious<CR>', opts)
+
 
